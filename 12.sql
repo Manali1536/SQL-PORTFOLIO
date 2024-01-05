ad2b7ccde03aabfc16e8b0d9cf66f35230579da2
Manali1536 <153725134+Manali1536@users.noreply.github.com>
2024-01-05 16:56:50 +0530

    Add files via upload

---
+++ 12.sql
@@ -0,0 +1,14 @@
+
+SELECT * FROM cars.`car_ dekho`;
+use cars;
+SELECT count(*) FROM cars.`car_ dekho` WHERE year = 2023; 
+SELECT year , count(*) `car_ dekho` FROM cars.`car_ dekho`
+ WHERE year in (2020,2021,2022)
+ group by year;
+ select count(*) from `car_ dekho`
+ where year = 2020 and fuel = "Diesel";
+ select*from cars. `car_ dekho`
+ 
+ 
+ 
+ 
